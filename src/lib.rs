dbf6b984a7f77b6a8f9c26a4810e9cc9970e89dd
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-11-23 18:04:55 +0900

    feat:test성공

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,11 @@
 use std::collections::HashMap;
-const Korea: [char; 13] = [
-    '가', '나', '다', '라', '마', '바', '사', '아', '자', '차', '타', '파', '하',
-];
-const ALPHABET: [char; 26] = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z',
-];
-
-pub fn alphbet_caesar(string: &str, shifts: u8) -> Result<String, Box<dyn std::error::Error>> {
-    let letters: HashMap<usize, &char> = ALPHABET.iter().enumerate().collect();
-    let numbers: HashMap<char, usize> = ALPHABET
+pub fn caesar(
+    string: &str,
+    shifts: u8,
+    lang_arr: Vec<char>,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let letters: HashMap<usize, &char> = lang_arr.iter().enumerate().collect();
+    let numbers: HashMap<char, usize> = lang_arr
         .iter()
         .enumerate()
         .map(|(idx, chr)| (chr.clone(), idx))
@@ -22,32 +18,8 @@ pub fn alphbet_caesar(string: &str, shifts: u8) -> Result<String, Box<dyn std::e
                 c
             } else {
                 let shift = numbers[&c] + shifts as usize;
-                if shift > 25 {
-                    *letters[&(shift % 26)]
-                } else {
-                    *letters[&shift]
-                }
-            }
-        })
-        .collect::<String>())
-}
-pub fn korean_caesar(string: &str, shifts: u8) -> Result<String, Box<dyn std::error::Error>> {
-    let letters: HashMap<usize, &char> = Korea.iter().enumerate().collect();
-    let numbers: HashMap<char, usize> = Korea
-        .iter()
-        .enumerate()
-        .map(|(idx, chr)| (chr.clone(), idx))
-        .collect();
-
-    Ok(string
-        .chars()
-        .map(|c| {
-            if c == ' ' {
-                c
-            } else {
-                let shift = numbers[&c] + shifts as usize;
-                if shift > 12 {
-                    *letters[&(shift % 13)]
+                if shift > lang_arr.len() - 1 {
+                    *letters[&(shift % lang_arr.len())]
                 } else {
                     *letters[&shift]
                 }
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,88 @@
-use Caesar::alphbet_caesar;
-use Caesar::korean_caesar;
-fn main() {
-    println!("{:?}", alphbet_caesar("fghij  abcde", 5).unwrap());
+use Caesar::caesar;
 
-    let test = alphbet_caesar("a", 1).unwrap();
-    let test2 = korean_caesar("가나다라마바사", 7).unwrap();
-    println!("{}", test);
-    println!("{}", test2);
+fn main() {
+    let korea_arr = vec![
+        '가', '나', '다', '라', '마', '바', '사', '아', '자', '차', '타', '파', '하',
+    ];
+    let alphbet_arr = vec![
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    ];
+    println!("{:?}", caesar("fghij  abcde", 5, korea_arr).unwrap());
+    println!("{:?}", caesar("fghij  abcde", 5, alphbet_arr).unwrap());
 }
 #[cfg(test)]
 mod test {
+
     use super::*;
     #[test]
     fn test() {
+        let alphbet_arr = vec![
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        ];
         assert_eq!(
-            alphbet_caesar("jgorevxumxgsskx", 20).unwrap(),
+            caesar("jgorevxumxgsskx", 20, alphbet_arr).unwrap(),
             String::from("dailyprogrammer")
         );
     }
     #[test]
     fn test1() {
-        assert_eq!(alphbet_caesar("a", 1).unwrap(), String::from("b"));
+        let alphbet_arr = vec![
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        ];
+        assert_eq!(caesar("a", 1, alphbet_arr).unwrap(), String::from("b"));
     }
     #[test]
     fn test2() {
-        assert_eq!(alphbet_caesar("abcz", 1).unwrap(), String::from("bcda"));
+        let alphbet_arr = vec![
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        ];
+        assert_eq!(
+            caesar("abcz", 1, alphbet_arr).unwrap(),
+            String::from("bcda")
+        );
     }
     #[test]
     fn test3() {
-        assert_eq!(alphbet_caesar("irk", 13).unwrap(), String::from("vex"));
+        let alphbet_arr = vec![
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        ];
+        assert_eq!(caesar("irk", 13, alphbet_arr).unwrap(), String::from("vex"));
     }
     #[test]
     fn test4() {
-        assert_eq!(alphbet_caesar("fusion", 6).unwrap(), String::from("layout"));
+        let alphbet_arr = vec![
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        ];
+        assert_eq!(
+            caesar("fusion", 6, alphbet_arr).unwrap(),
+            String::from("layout")
+        );
     }
     #[test]
     fn test5() {
+        let alphbet_arr = vec![
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        ];
         assert_eq!(
-            alphbet_caesar("dailyprogrammer", 6).unwrap(),
+            caesar("dailyprogrammer", 6, alphbet_arr).unwrap(),
             String::from("jgorevxumxgsskx")
         );
     }
     #[test]
     fn test6() {
+        let alphbet_arr = vec![
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        ];
         assert_eq!(
-            alphbet_caesar("jgorevxumxgsskx", 20).unwrap(),
+            caesar("jgorevxumxgsskx", 20, alphbet_arr).unwrap(),
             String::from("dailyprogrammer")
         );
     }
