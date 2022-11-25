a5b990988a929350f6ccf8ded247e42ba43e1950
kyunghyunHan <ekfqlwcjswl@naver.com>
2022-11-25 10:39:43 +0900

    ㅁㅇㅇ

--- src/lib.rs
+++ src/lib.rs
@@ -4,26 +4,36 @@ pub fn encryption(
     shifts: u8,
     lang_arr: Vec<char>,
 ) -> Result<String, Box<dyn std::error::Error>> {
+    //현재 반복 횟수와 다음 값을 제공하는 반복자를 만듬.
+    //반환된 반복자는 i,val생성
     let letters: HashMap<usize, &char> = lang_arr.iter().enumerate().collect();
     let numbers: HashMap<char, usize> = lang_arr
         .iter()
         .enumerate()
         .map(|(idx, chr)| (chr.clone(), idx))
         .collect();
-
+    //chars:문자열 조각의 [char]s에 대한 반복자를 반환
     Ok(string
         .chars()
         .map(|c| {
+            //c가 띄우쓰기면 그대로 반환
             if c == ' ' {
                 c
             } else {
+                //shifts를 usize로 변경후 number  shifts 만큼이동
+                //Hashmap에서 "a"를 찾아서 usize만큼 이동
                 let shift = numbers[&c] + shifts as usize;
+                //shift가 배열의 -1보다 크면
+                //정렬
                 if shift > lang_arr.len() - 1 {
+                    //letter shift % 배열의 크기만큼으로 변경
                     *letters[&(shift % lang_arr.len())]
                 } else {
+                    //아니면 그위치
                     *letters[&shift]
                 }
             }
         })
+        //스트링으로 반환
         .collect::<String>())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
+use std::collections::HashMap;
 use Caesar::encryption;
-
 fn main() {
+    //다음반복 횟수와 다음 값을 제공하는 반복자
+    // let letters: HashMap<usize, &char> = tes2.iter().enumerate().collect();
+
     let korea_arr = vec![
-        '가', '나', '다', '라', '마', '바', '사', '아', '자', '차', '타', '파', '하',
+        '가', '나', '다', '라', '마', '바', '사', '아', '자', '차', '카', '타', '파', '하',
     ];
     let alphbet_arr = vec![
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
         's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     ];
-    println!("{:?}", encryption("fghij  abcde", 5, korea_arr).unwrap());
-    println!("{:?}", encryption("fghij  abcde", 5, alphbet_arr).unwrap());
+    println!(
+        "{:?}",
+        encryption("나다라마 바사아자", 5, korea_arr).unwrap()
+    );
+
+    let dd = vec!['일', '이', '삼', '사', '오'];
+    // println!("{:?}", encryption("fghij    abcde", 5, alphbet_arr).unwrap());
+    let letters: HashMap<usize, &char> = dd.iter().enumerate().collect();
+    let numbers: HashMap<char, usize> = dd
+        .iter()
+        .enumerate()
+        .map(|(idx, chr)| (chr.clone(), idx))
+        .collect();
+    println!("{:?}", numbers);
 }
 #[cfg(test)]
 mod test {
